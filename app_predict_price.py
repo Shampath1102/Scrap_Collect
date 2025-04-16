791fb6cd7512a5e444156107345134f3a568171b
Shampath1102 <162000830+Shampath1102@users.noreply.github.com>
2025-04-16 21:09:34 +0530

    Update app_predict_price.py

--- app_predict_price.py
+++ app_predict_price.py
@@ -17,8 +17,8 @@ os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 
 # Load models
-scrap_type_model = load_model("C:/Users/rupa1/OneDrive/Desktop/New LSM App/model_python/scrap_type_model.h5")
-condition_model = load_model("C:/Users/rupa1/OneDrive/Desktop/New LSM App/model_python/condition_model_binary.h5")
+scrap_type_model = load_model("C:/Users/rupa1/OneDrive/Desktop/New LSM App/Frontend/scrap_type_model.h5")
+condition_model = load_model("C:/Users/rupa1/OneDrive/Desktop/New LSM App/Frontend/condition_model_binary.h5")
 
 # Load scrap type class indices
 with open("C:/Users/rupa1/OneDrive/Desktop/New LSM App/model_python/class_indices.json", "r") as f:
