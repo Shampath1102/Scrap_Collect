91d3d3fbcb012d6e72fb9cd61834d6d9de07be56
Shampath1102 <162000830+Shampath1102@users.noreply.github.com>
2025-04-17 10:33:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -158,8 +158,11 @@ def create_db():
         db.create_all()
         print("✅ Database created successfully")
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port)
+
 
 
 
